c557aa9dd5c5fcf681e34836bd58e12df61e0fad
CheezItMan <chris@adadevelopersacademy.org>
2018-11-26 11:18:51 -0800

    added student component

--- src/App.js
+++ src/App.js
@@ -1,68 +1,22 @@
+// Import Library Stuff
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// My Components
+import Student from './components/Student';
+// Non Component Stuff
 import './App.css';
 
 class App extends Component {
   
-  isAgeFive(num) {
-    if (num === 5) {
-      return (
-        <div>
-          You are exactly the right age for Kindergarten!
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          You are not the right age for Kindergarten!
-        </div>
-      )
-    }
-  }
-
   render() {
-    const names = ['Dee', 'Dan', 'Devin'];
-    const num = 5;
-
-    // if (num === 6) {
-    //   return (
-    //     <div>
-    //       Wonderful JSX
-    //     </div>
-    //   )
-    // }
-    // else {
-    //   return (
-    //     <div>
-    //       Not so wonderful JSX
-    //     </div>
-    //   )
-    // }
-    // this is a comment
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <img 
-            src="https://www.fast-growing-trees.com/images/P/Sawtooth-Oak_450_a.jpg" 
-          />
+      <main className="app">
+        <h1>My Great React App</h1>
+        <Student />
+        <Student />
+        <Student />
 
-          <br />
-          {num}
-          {names.map((name) => <li>{name}</li>)}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            { this.isAgeFive(num)  }
-          </a>
-        </header>
-      </div>
-    );
+      </main>
+    )
   }
 }
 
---
+++ src/components/Student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Student = () => {
+    return (
+        <section>
+            <h2>Student Component Here!</h2>
+        </section>
+    )
+};
+
+export default Student;
